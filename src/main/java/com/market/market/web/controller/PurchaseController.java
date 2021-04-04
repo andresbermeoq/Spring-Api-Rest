92e13c0c3e1f9b75d08d8145356efd44e06dbdf0
andipandiber <andipandi467@gmail.com>
2021-04-04 05:28:34 -0500

    Add JWT Security

--- src/main/java/com/market/market/web/controller/PurchaseController.java
+++ src/main/java/com/market/market/web/controller/PurchaseController.java
@@ -2,6 +2,10 @@ package com.market.market.web.controller;
 
 import com.market.market.domain.Purchase;
 import com.market.market.domain.service.PurchaseService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -17,18 +21,28 @@ public class PurchaseController {
     private PurchaseService purchaseService;
 
     @GetMapping("/all")
+    @ApiOperation("Get all supermarket purchases")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Purchase>> getAll() {
         return new ResponseEntity<>(purchaseService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/client/{id}")
-    public ResponseEntity<List<Purchase>> getByClient(@PathVariable("id") String clientId) {
+    @ApiOperation("Search a purchase with an client ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "PURCHASE NOT FOUND")
+    })
+    public ResponseEntity<List<Purchase>> getByClient(@ApiParam(value = "The ID of the purchase", required = true, example = "1")
+                                                          @PathVariable("id") String clientId) {
         return purchaseService.getByClient(clientId)
                 .map(purchases -> new ResponseEntity<>(purchases, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
+    @ApiOperation("Save a purchase in the database")
+    @ApiResponse(code = 201, message = "PURCHASE CREATED")
     public ResponseEntity<Purchase> save(@RequestBody Purchase purchase) {
         return new ResponseEntity<>(purchaseService.save(purchase), HttpStatus.CREATED);
     }
