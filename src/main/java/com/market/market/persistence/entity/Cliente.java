05c9de1175494f1176010fc5340c460d98ed1552
andipandiber <andipandi467@gmail.com>
2021-04-04 04:49:49 -0500

    Add Api Swagger Documentation

--- src/main/java/com/market/market/persistence/entity/Cliente.java
+++ src/main/java/com/market/market/persistence/entity/Cliente.java
@@ -9,10 +9,10 @@ public class Cliente {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private String id;
     private String nombre;
     private String apellidos;
-    private Integer celular;
+    private Long celular;
     private String direccion;
     @Column(name = "correo_electronico")
     private String correo_electronico;
@@ -20,11 +20,11 @@ public class Cliente {
     @OneToMany(mappedBy = "cliente")
     private List<Compra> compras;
 
-    public Integer getId() {
+    public String getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(String id) {
         this.id = id;
     }
 
@@ -44,11 +44,11 @@ public class Cliente {
         this.apellidos = apellidos;
     }
 
-    public Integer getCelular() {
+    public Long getCelular() {
         return celular;
     }
 
-    public void setCelular(Integer celular) {
+    public void setCelular(Long celular) {
         this.celular = celular;
     }
 
--- src/main/java/com/market/market/web/controller/ProductController.java
+++ src/main/java/com/market/market/web/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.market.market.web.controller;
 
 import com.market.market.domain.Product;
 import com.market.market.domain.service.ProductService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,30 +19,51 @@ public class ProductController {
     private ProductService productService;
 
     @GetMapping("/all")
+    @ApiOperation("Get all supermarket products")
+    @ApiResponse(code = 200, message = "OK")
     public ResponseEntity<List<Product>>  getAll() {
         return new ResponseEntity<>(productService.getAll(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> getProduct(@PathVariable("id") int productId) {
+    @ApiOperation("Search a product with an ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "PRODUCT NOT FOUND")
+    })
+    public ResponseEntity<Product> getProduct(@ApiParam(value = "The ID of the product", required = true, example = "7")
+                                                  @PathVariable("id") int productId) {
         return productService.getProduct(productId)
                 .map(product -> new ResponseEntity<>(product, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @GetMapping("/category/{id}")
-    public ResponseEntity<List<Product>> getByCategory(@PathVariable("id") int categoryId) {
+    @ApiOperation("Search a category with an ID")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "CATEGORY NOT FOUND")
+    })
+    public ResponseEntity<List<Product>> getByCategory(@ApiParam(value = "The ID of the category", required = true, example = "3")
+                                                           @PathVariable("id") int categoryId) {
         return productService.getByCategory(categoryId)
                 .map(products -> new ResponseEntity<>(products, HttpStatus.OK))
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @PostMapping("/save")
+    @ApiOperation("Save a product in the database")
+    @ApiResponse(code = 201, message = "PRODUCT CREATED")
     public ResponseEntity<Product> save(@RequestBody Product product) {
         return new ResponseEntity<>(productService.save(product), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/delete/{id}")
+    @ApiOperation("Delete a product in the database")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "OK"),
+            @ApiResponse(code = 404, message = "PRODUCT NOT FOUND")
+    })
     public ResponseEntity delete(@PathVariable("id") int productId) {
         if (productService.delete((productId))) {
             return new ResponseEntity(HttpStatus.OK);
